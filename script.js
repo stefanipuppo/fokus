85e10d94674b83b560732eb0ddad8251f83ed4cf
Stefani Dal Puppo <stefanipuppo@hotmail.com>
2024-03-26 19:28:58 -0300

    Inicializando e pausado sons + alternando texto com textContent

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('/sons/beep.mp3')
+
+let tempoDecorridoEmSegundos = 5
+let intervalorId = null
+
 musica.loop = true
 
 musicaFocoInput.addEventListener('change', () => {
@@ -53,4 +62,34 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+       
+        alert('tempo finalizado')
+        zerar()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervalorId){
+        audioPausa
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervalorId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = "Pausar"
+}
+
+function zerar() {
+    clearInterval(intervalorId)
+    iniciarOuPausarBt.textContent = "Começar"
+    intervalorId = null
 }
